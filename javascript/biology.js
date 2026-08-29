260bad95f91eea274607dfcc3025496bed4b7a59
israel almarales <israelalmarales@icloud.com>
2026-08-29 05:27:48 -0400

    commit

--- javascript/biology.js
+++ javascript/biology.js
@@ -1,3 +1,15 @@
-const dropdownItem = document.querySelector('.dropdown');
+//JavaScript style for section1 of biology
+const dropdownItem = document.querySelector('#section1');
+dropdownItem.style.backgroundColor = 'pink';
+dropdownItem.style.color = 'red';
+dropdownItem.style.borderRadius = '12px';
+dropdownItem.style.fontStyle = 'italic';
+
+//JavaScript style for option elements of section1 biology
+const listItems = document.querySelectorAll('option');
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.fontSize = '5rem';
+}
 
 
+//JavaScript style for linking sidebar option element selection to specific document
